aaac55aa2bfe7dadd7886663e54b1e4aa2f8cf2b
Rico Apon <rico.apon@gmail.com>
2023-02-18 17:09:58 +0100

    Remove unused import

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/markdown/markdown';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
