3fc5f4982d422e4339d0e48d73d9e978dd7d4d39
Rico Apon <rico.apon@gmail.com>
2023-03-02 18:18:47 +0100

    Add levels with description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {Parser} from "./backend/parser";
-import {Machine} from "./backend/machine";
-import {Level, LEVEL_1, LEVEL_2} from "./backend/levels";
-import {MachineGuiExecutor} from "./backend/machine-gui-executor";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +6,5 @@ import {MachineGuiExecutor} from "./backend/machine-gui-executor";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // readonly level: Level = LEVEL_2;
-  // title = 'humans-clone';
-  // machineGuiExecutor: MachineGuiExecutor;
-  //
-  // ngAfterViewInit(): void {
-  //   this.svgGridComponent.initialize(this.level)
-  //   this.svgGridComponent.detectChanges()
-  //   this.machineGuiExecutor = new MachineGuiExecutor(this.level, this.svgGridComponent)
-  // }
-  //
-  // execute(input: any) {
-  //   const guiActions = new Parser(new Machine(this.level)).parse(input)
-  //   this.machineGuiExecutor.execute(guiActions)
-  // }
+
 }
--- src/app/backend/levels.ts
+++ src/app/backend/levels.ts
@@ -2,29 +2,48 @@ export class Level {
   readonly input: number[];
   readonly expectedOut: number[];
   readonly nrOfMemorySlots: number;
+  readonly description: string;
 
-  constructor(input: number[], expectedOut: number[], nrOfMemorySlots: number) {
+  constructor(input: number[], expectedOut: number[], nrOfMemorySlots: number, description: string) {
     this.input = input;
     this.expectedOut = expectedOut;
     this.nrOfMemorySlots = nrOfMemorySlots;
+    this.description = description;
   }
 }
 
-export const LEVEL_1 = new Level([9, 6, 4], [9, 6, 4], 1)
-
-// Reverse two inputs and put them into the output. Repeat.
-export const LEVEL_2 = new Level([4, 5, 1, 7, 8, 9], [5, 4, 7, 1, 8, 9], 3)
-
-// Put the sum of two elements in the output. Repeat.
-export const LEVEL_3 = new Level([3, 6, 6, 8, -1, 8, 5, -5], [9, 14, 7, 0], 3)
+const LEVELS: Level[] = [
+  new Level([9, 6, 4], [9, 6, 4], 0,
+    "Write commands in the editor below to build a program. Your task is to use the numbers (the purple boxes) in the input to " +
+    "get the expected output. In this first simple program, you can move the input to the output without modifications.<br>" +
+    "<br>" +
+    "You can use the command <code>move input to output</code>."),
+  new Level([9, 6, 4, 9, 6, 4, 9, 6, 4, 9, 6, 4], [9, 6, 4, 9, 6, 4, 9, 6, 4, 9, 6, 4], 0,
+    "Writing out every line by hand is very tedious work. We can do that far easier: use a loop! Try and move " +
+    "all the input numbers to the output with only four lines of code!<br>" +
+    "<br>" +
+    "You can use the command <code>while (input not empty) { ... }</code> to loop."),
+  new Level([4, 5, 1, 7, 8, 9], [5, 4, 7, 1, 8, 9], 3,
+    "If every level was just moving the input to the output, this game would be too simple! Let's switch it up: " +
+    "you have to grab the first TWO numbers from the input and move them to the output in REVERSE order.<br>" +
+    "<br>" +
+    "The tiles you can see in the middle are called 'memory slots'. The number you can see on the memory slot is called " +
+    "index. You can store numbers in memory slots. You should use them.<br>" +
+    "<br>" +
+    "You can use the command 'move X to Y' to move numbers from X to Y. You can either use 'input', 'output' or the index " +
+    "of a memory slot. For example, 'move input to 0' would move a number from the input to the " +
+    "memory slot with the number 0."),
+  new Level([3, 6, 6, 8, -1, 8, 5, -5], [9, 14, 7, 0], 3,
+    "It is now time to modify the numbers using the power of addition! For each two things in the input, add " +
+    "them together and put the result in the output.<br>" +
+    "<br>" +
+    "You can use the command 'add X to Y' where X and Y must be memory slots.")
+]
 
 export function getLevel(i: number): Level {
-  if (i == 1) {
-    return LEVEL_1
-  } else if (i == 2) {
-    return LEVEL_2
-  } else if (i == 3) {
-    return LEVEL_3
+  if (i <= 0 || i > LEVELS.length) {
+    throw Error("Level " + i + " does not exist!")
   }
-  throw Error("Level " + i + " does not exist!")
+
+  return LEVELS[i - 1];
 }
