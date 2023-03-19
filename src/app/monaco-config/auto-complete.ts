faffecaf33219a5550eba702b7aec895e1d45d0a
Rico Apon <rico.apon@gmail.com>
2023-03-19 15:08:02 +0100

    Create hover and suggestions based on a single list

--- src/app/monaco-config/auto-complete.ts
+++ src/app/monaco-config/auto-complete.ts
@@ -30,73 +30,123 @@ export function loadAutoComplete(monaco: any, languageName: string) {
       // To have compilation checks, we will just fill in the range anyway.
       const range = createRange(model, position)
 
-      const suggestions = [
-        {
-          label: 'simpleText',
-          kind: monaco.languages.CompletionItemKind.Text,
-          insertText: 'simpleText',
-          range
-        },
-        {
-          label: 'inbox2',
-          kind: monaco.languages.CompletionItemKind.Keyword,
-          insertText: 'testing(${1:condition})',
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          range,
-        },
-        {
-          label: 'loop',
-          kind: monaco.languages.CompletionItemKind.Snippet,
-          insertText: 'loop {\n  $0\n}',
-          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          documentation: {
-            value: `Look at this cool markdown!`,
-            isTrusted: true
-          },
-          detail: "this is just a loop",
-          range,
-        },
-        {
-          label: 'inbox',
-          kind: monaco.languages.CompletionItemKind.Text,
-          insertText: 'inbox',
-          documentation: {
-            value: `That thing on the left with all the boxes!`,
-            isTrusted: true
-          },
-          detail: "the thingie on the left",
-          range,
-        },
-        {
-          label: 'CityJS',
-          kind: monaco.languages.CompletionItemKind.Constant,
-          insertText: 'CityJS',
-          documentation: {
-            value: `Look at this cool markdown!`,
-            isTrusted: true
-          },
-          range,
-        }
-      ];
-      return { suggestions: suggestions };
+      return {suggestions: createSuggestions(monaco, range)};
     }
   }
   monaco.languages.registerCompletionItemProvider(languageName, completionItemProvider);
 
-  // Wherever you hover, you now see some documentation!
   const hoverProvider: HoverProvider = {
     provideHover: (model: ITextModel, position: Position) => {
+      const currentWord = model.getWordAtPosition(position);
+      if (currentWord == null) {
+        return {contents: []};
+      }
+      const content = createHoverDocumentation(currentWord.word, monaco);
+      if (content == undefined) {
+        return {contents: []};
+      }
+
       const range = createRange(model, position)
       return {
         range,
-        contents: [
-          {
-            value: `Look at this documentation!`,
-            isTrusted: true
-          }
-        ]
+        contents: [content]
       }
     }
   }
   monaco.languages.registerHoverProvider(languageName, hoverProvider)
+}
+
+export type DictionaryItem = {
+  /* The word that must match the word on screen to show this. */
+  label: string
+  /* A long text providing documentation on the item. */
+  documentation: string
+  /* The content of the suggestion, if this can be a suggestion. */
+  suggestion?: DictionaryItemSuggestion
+}
+
+export type DictionaryItemSuggestion = {
+  kind: any
+  insertText: string
+  insertTextRules?: any
+}
+
+function createSuggestions(monaco: any, range: any): languages.CompletionItem[] {
+  return createDictionary(monaco)
+    .filter(item => item.suggestion != undefined)
+    .map(item => {
+        return {
+          label: item.label,
+          kind: item.suggestion!.kind,
+          insertText: item.suggestion!.insertText,
+          insertTextRules: item.suggestion!.insertTextRules,
+          documentation: {
+            value: item.documentation,
+            isTrusted: true
+          },
+          range
+        }
+      }
+    )
+}
+
+function createHoverDocumentation(word: string, monaco: any): {value: string, isTrusted: boolean} | undefined {
+  const result = createDictionary(monaco)
+    .filter(item => item.label == word)
+    .map(item => {
+        return {
+          value: item.documentation,
+          isTrusted: true
+        }
+      }
+    );
+  if (result.length > 1) {
+    throw new Error('Every label should be unique, but we found multiple results for a single word ("' + word + '"). Fix the dictionary.')
+  }
+  if (result.length == 0) {
+    return undefined;
+  }
+  return result[0];
+}
+
+function createDictionary(monaco: any): DictionaryItem[] {
+  return [
+    {
+      label: "move",
+      documentation: "#### move [memory-slot | input] to [memory-slot | output]\n---\n" +
+        "This action makes it possible to move memory blocks from one place to another.",
+      suggestion: {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'move $1 to $2',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,      }
+    },
+    {
+      label: 'loop',
+      documentation: "#### loop { [code block] }\n---\n" +
+        "Repeats the nested code block until the `break` command is used.",
+      suggestion: {
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        insertText: 'loop {\n  $0\n}',
+        insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+      }
+    },
+    {
+      label: 'input',
+      documentation: "#### input\n---\n" +
+        "Represents the list that contains all the memory blocks that are the input for the level.",
+      suggestion: {
+        kind: monaco.languages.CompletionItemKind.Text,
+        insertText: 'input',
+      }
+    },
+    {
+      label: 'output',
+      documentation: "#### output\n---\n" +
+        "Represents the list that contains all the memory blocks that are the output of the level.",
+      suggestion: {
+        kind: monaco.languages.CompletionItemKind.Text,
+        insertText: 'output',
+      }
+    },
+  ]
 }
