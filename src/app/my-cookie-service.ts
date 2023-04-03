81e219b4552f9e481aadee0d9dc8d114407de046
Rico Apon <rico.apon@gmail.com>
2023-04-03 22:31:53 +0200

    Add cookie settings that I will implement later

--- src/app/my-cookie-service.ts
+++ src/app/my-cookie-service.ts
@@ -9,6 +9,9 @@ export class MyCookieService {
   readonly SPEED_UP_FACTOR = 'speedUpFactor'
   readonly SPEED_UP_FACTOR_DEFAULT = 1
 
+  readonly COMPLETED_LEVEL_PREFIX = 'completedLevel_'
+  readonly COMPLETED_CODE_CHALLENGE_PREFIX = 'completedCodeChallenge_'
+
   constructor(private cookieService: CookieService) {
   }
 
@@ -21,5 +24,21 @@ export class MyCookieService {
 
   setSpeedUpFactor(value: number) {
     this.cookieService.set(this.SPEED_UP_FACTOR, String(value), 365)
+  }
+
+  hasCompletedLevel(levelId: number): boolean {
+    return this.cookieService.check(this.COMPLETED_LEVEL_PREFIX + levelId)
+  }
+
+  setCompletedLevel(levelId: number) {
+    this.cookieService.set(this.COMPLETED_LEVEL_PREFIX + levelId, String('true'), 365)
+  }
+
+  hasCompletedCodeChallenge(levelId: number): boolean {
+    return this.cookieService.check(this.COMPLETED_CODE_CHALLENGE_PREFIX + levelId)
+  }
+
+  setCompletedCodeChallenge(levelId: number) {
+    this.cookieService.set(this.COMPLETED_CODE_CHALLENGE_PREFIX + levelId, String('true'), 365)
   }
 }
