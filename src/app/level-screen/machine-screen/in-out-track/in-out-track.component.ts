762ee1cfa0519b70b72fd08f0c8b226778af5dd2
Rico Apon <rico.apon@gmail.com>
2023-04-09 15:17:02 +0200

    Fix issue

--- src/app/level-screen/machine-screen/in-out-track/in-out-track.component.ts
+++ src/app/level-screen/machine-screen/in-out-track/in-out-track.component.ts
@@ -1,14 +1,17 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-in-out-track',
   templateUrl: './in-out-track.component.html',
   styleUrls: ['./in-out-track.component.css']
 })
-export class InOutTrackComponent {
+export class InOutTrackComponent implements AfterViewChecked {
   @Input() isIn: boolean;
   @Input() values: number[] = []
   @ViewChild('line') line: ElementRef;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+  }
 
   maxIndex(): number {
     if (this.line == undefined) {
@@ -19,5 +22,11 @@ export class InOutTrackComponent {
     // // Each block and gap is 64 pixels,
     // const nrOfBlocksToShow = (+this.line.style.width) / 128;
     // return nrOfBlocksToShow;
+  }
+
+  ngAfterViewChecked(): void {
+    // We use two instances of this component in the same screen. For some reason, change detection gives the error
+    // "Expression has changed after it was checked". To resolve this, we add this line.
+    this.changeDetectorRef.detectChanges()
   }
 }
